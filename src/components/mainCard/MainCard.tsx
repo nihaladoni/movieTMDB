4a33ada69fd8e2f96947192b28ae19f9a974d773
nihaladoni <nihaladoni@gmail.com>
2023-08-29 02:37:50 +0530

    fix: loader in hero section

--- src/components/mainCard/MainCard.tsx
+++ src/components/mainCard/MainCard.tsx
@@ -3,9 +3,10 @@ import { useEffect, useState } from 'react'
 import { useGetTrending } from '../../hooks/useTrending'
 import { img_1280, img_300 } from '../../utils/apiUrl'
 import * as Styled from './MainCard.style'
+import LoadingSpinner from '../uploadSpinner/LoadingSpinner'
 
 const MainCard = () => {
-  const { data } = useGetTrending()
+  const { data, isFetching } = useGetTrending()
   const [currentMovie, setCurrentMovie] = useState({
     backdrop_path: '',
     original_title: '',
@@ -23,16 +24,22 @@ const MainCard = () => {
 
   return (
     <Styled.MainCardContainer bgUrl={img_1280 + currentMovie?.backdrop_path}>
-      <Styled.MovieInfoContainer>
-        <Styled.InfoDiv>
-          <h1>{currentMovie?.original_title || currentMovie?.original_name}</h1>
-          <p>{currentMovie?.overview}</p>
-        </Styled.InfoDiv>
+      {isFetching ? (
+        <LoadingSpinner innerSize='20' outerSize='50' />
+      ) : (
+        <Styled.MovieInfoContainer>
+          <Styled.InfoDiv>
+            <h1>
+              {currentMovie?.original_title || currentMovie?.original_name}
+            </h1>
+            <p>{currentMovie?.overview}</p>
+          </Styled.InfoDiv>
 
-        <Styled.ImageContainer>
-          <img src={img_300 + currentMovie?.poster_path} alt='poster image' />
-        </Styled.ImageContainer>
-      </Styled.MovieInfoContainer>
+          <Styled.ImageContainer>
+            <img src={img_300 + currentMovie?.poster_path} alt='poster image' />
+          </Styled.ImageContainer>
+        </Styled.MovieInfoContainer>
+      )}
     </Styled.MainCardContainer>
   )
 }
